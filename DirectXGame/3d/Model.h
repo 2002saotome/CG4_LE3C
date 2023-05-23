2aa3c522921cb1fd1cbfa134333af2a0f7f4cd60
2002五月女 <94036178+2002saotome@users.noreply.github.com>
2023-05-23 10:58:34 +0900

    完成

--- DirectXGame/3d/Model.h
+++ DirectXGame/3d/Model.h
@@ -1,5 +1,6 @@
 #include<string>
 #include<DirectXMath.h>
+#include<DirectXTex.h>
 #include<vector>
 
 //ノード
@@ -27,9 +28,32 @@ public:
 	//フレンドクラス
 	friend class FbxLoader;
 private:
-	
 	//モデル名
 	std::string name;
 	//ノード配列
 	std::vector<Node> nodes;
+public: //サブクラス
+	//頂点データ構造体
+	struct VertexPosNormalUv
+	{
+		DirectX::XMFLOAT3 pos; //x,y,z座標
+		DirectX::XMFLOAT3 normal; //法線ベクトル
+		DirectX::XMFLOAT2 uv; //uv座標
+	};
+
+	//メッシュを持つノード
+	Node* meshNode = nullptr;
+	//頂点データ配列
+	std::vector<VertexPosNormalUv> vertices;
+	//頂点インデックス配列
+	std::vector<unsigned short>indices;
+
+	//アンビエント係数
+	DirectX::XMFLOAT3 ambient = { 1,1,1 };
+	//ディフューズ係数
+	DirectX::XMFLOAT3 diffuse = { 1,1,1 };
+	//テクスチャメタデータ
+	DirectX::TexMetadata metadata = {};
+	//スクラッチイメージ
+	DirectX::ScratchImage scratchImg = {};
 };
