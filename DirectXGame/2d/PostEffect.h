d2ec978ed7203312359e705d8bc7078777504306
2002五月女 <94036178+2002saotome@users.noreply.github.com>
2023-06-28 16:32:31 +0900

    完成

--- DirectXGame/2d/PostEffect.h
+++ DirectXGame/2d/PostEffect.h
@@ -3,16 +3,37 @@
 class PostEffect :
     public Sprite
 {
+
+private: // エイリアス
+    // Microsoft::WRL::を省略
+    template <class T> using ComPtr = Microsoft::WRL::ComPtr<T>;
+    // DirectX::を省略
+    using XMFLOAT2 = DirectX::XMFLOAT2;
+    using XMFLOAT3 = DirectX::XMFLOAT3;
+    using XMFLOAT4 = DirectX::XMFLOAT4;
+    using XMVECTOR = DirectX::XMVECTOR;
+    using XMMATRIX = DirectX::XMMATRIX;
+
 public:
     ///<summary>
     ///コンストラクタ
     ///</summary>
-    PostEffect();
+    PostEffect(); 
+    
+    ///<summary>
+    ///初期化
+    ///</summary>
+    void Initialize();
 
     ///<summary>
     ///描画コマンドの発行
     ///</summary>
     /// <param name="cmdList">コマンドリスト</param>
     void Draw(ID3D12GraphicsCommandList* cmdList);
+    
+   //テクスチャバッファ
+    ComPtr<ID3D12Resource> texBuff;
+    //SRVデスクリプタヒープ
+    ComPtr<ID3D12DescriptorHeap> descHeapSRV;
 };
 
