4eb8c68556b5202859e49518fe02ee849720ff49
2002五月女 <94036178+2002saotome@users.noreply.github.com>
2023-06-29 11:15:20 +0900

    完成

--- DirectXGame/2d/PostEffect.h
+++ DirectXGame/2d/PostEffect.h
@@ -30,10 +30,32 @@ public:
     ///</summary>
     /// <param name="cmdList">コマンドリスト</param>
     void Draw(ID3D12GraphicsCommandList* cmdList);
-    
+
+    ///<summary>
+    ///シーン描画前処理
+    ///</summary>
+    /// <param name="cmdList">コマンドリスト</param>
+    void PreDrawScene(ID3D12GraphicsCommandList* cmdList);
+
+    ///<summary>
+    ///シーン描画後処理
+    ///</summary>
+    /// <param name="cmdList">コマンドリスト</param>
+    void PostDrawScene(ID3D12GraphicsCommandList* cmdList);
+
    //テクスチャバッファ
     ComPtr<ID3D12Resource> texBuff;
     //SRVデスクリプタヒープ
     ComPtr<ID3D12DescriptorHeap> descHeapSRV;
+    //深度バッファ
+    ComPtr<ID3D12Resource> depthBuff;
+    //RTV用デスクリプタヒープ
+    ComPtr<ID3D12DescriptorHeap> descHeapRTV;
+    //DSV用デスクリプタヒープ
+    ComPtr<ID3D12DescriptorHeap> descHeapDSV;
+
+private:
+    //画面クリアカラー
+    static const float clearColor[4];
 };
 
