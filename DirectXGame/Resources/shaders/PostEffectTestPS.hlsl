9229727334ab86bb6ad9924f96d6339548e3a50c
2002五月女 <94036178+2002saotome@users.noreply.github.com>
2023-07-06 10:43:25 +0900

    完成

--- DirectXGame/Resources/shaders/PostEffectTestPS.hlsl
+++ DirectXGame/Resources/shaders/PostEffectTestPS.hlsl
@@ -1,15 +1,25 @@
 #include "PostEffectTest.hlsli"
 
-Texture2D<float4> tex : register(t0);  // 0番スロットに設定されたテクスチャ
+Texture2D<float4> tex0 : register(t0);  // 0番スロットに設定されたテクスチャ
+Texture2D<float4> tex1 : register(t1); // 1番スロットに設定されたテクスチャ
 SamplerState smp : register(s0);      // 0番スロットに設定されたサンプラー
 
 float4 main(VSOutput input) : SV_TARGET
 {
-    
-    // UV指定したピクセルの色をサンプリング(UVずらし)
-    float4 texcolor = tex.Sample(smp,input.uv);
-    //アルファに1を入れて出力(UVずらし)
-    return float4(texcolor.rgb, 1);
-    
+    // UV指定したピクセルの色をサンプリング
+    //float4 texcolor = tex.Sample(smp,input.uv);
+    //アルファに1を入れて出力
+    //return float4(texcolor.rgb, 1);
+
+    float4 colortex0 = tex0.Sample(smp,input.uv);
+    float4 colortex1 = tex1.Sample(smp, input.uv);
+
+    float4 color = colortex0;
+    if (fmod(input.uv.y, 0.1f) < 0.05f)
+    {
+        color = colortex1;
+    }
+
+    return float4(color.rgb, 1);
    
 }
